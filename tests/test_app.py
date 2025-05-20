d76239268ec2b30cf59046a01b46ae6f1f481c1e
CarlosHenriquePSouza <carloshps@gmail.com>
2025-05-19 21:19:43 -0300

    Adicionando schema de mensagem

--- tests/test_app.py
+++ tests/test_app.py
@@ -22,3 +22,10 @@ def test_root_deve_retornar_ola_mundo():
     # Assert
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_html():
+    client = TestClient(app)
+    response = client.get('/html')
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1>Olá Mundo!</h1>' in response.text
